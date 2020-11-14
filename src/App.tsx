621e9f614506cfff86d6efa5548d03dc222bff3a
Pablo Núñez <pablonete@github.com>
2020-11-15 00:23:09 +0100

    Add UI to start/stop recording.

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
         />
         <div>Unsayo</div>
       </header>
-      <section>
+      <section className="padding-16">
         <SessionPage />
       </section>
     </div>
--- src/Views/SessionPage.tsx
+++ src/Views/SessionPage.tsx
@@ -1,5 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+
+interface PlayRecord {
+  title: string;
+  start: Date;
+  end: Date;
+}
 
 export function SessionPage() {
-  return <h1>Session Page</h1>;
+  const [title, setTitle] = useState("<untitled>");
+  const [start, setStart] = useState<Date>(new Date());
+  const [recording, setRecording] = useState(false);
+  const [records, setRecords] = useState<PlayRecord[]>([]);
+
+  return (
+    <div className="flex-column">
+      <h1>Sesión de hoy</h1>
+      <div className="flex-row rhythm-h-16">
+        <div className="flex-column flex-grow">
+          <label>Título</label>
+          <input
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+            value={title}
+          />
+          <div>
+            <button
+              onClick={() => {
+                if (!recording) {
+                  setStart(new Date());
+                } else {
+                  setRecords([...records, { start, end: new Date(), title }]);
+                }
+
+                setRecording(!recording);
+              }}
+            >
+              {recording ? "Terminar" : "Empezar"}
+            </button>
+          </div>
+        </div>
+        <div className="flex-column flex-grow">
+          <h3>Interpretaciones</h3>
+          {records.map((record) => (
+            <div>
+              {record.title} at {record.start.toLocaleTimeString()} for 99s
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
