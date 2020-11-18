400990baa3a3bb1bca5fa3aacea15d90f9ebff5d
Pablo Núñez <pablonete@github.com>
2020-11-18 22:45:45 +0100

    Update icon and manifest.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from "react";
-import logo from "./images/nicubunu_Musical_note.svg";
+import logo from "./images/oboito.png";
 import { SessionPage } from "./Views/SessionPage";
 
 function App() {
   return (
     <div>
       <header className="flex-row flex-center color-header font-xl">
-        <img
-          src={logo}
-          className="image-height-l infinite-spin margin-8"
-          alt="logo"
-        />
+        <img src={logo} className="image-height-l margin-16" alt="logo" />
         <div>Unsayo</div>
       </header>
       <section className="padding-16">
