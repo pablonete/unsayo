07830beba1a3e01d06ff68fb9ad1b051d5d355d5
Pablo Núñez <pablonete@github.com>
2020-11-18 01:13:21 +0100

    Refactor to components.

--- src/Views/SessionPage.tsx
+++ src/Views/SessionPage.tsx
@@ -10,10 +10,30 @@ interface PlayRecord {
 }
 
 export function SessionPage() {
+  const [records, setRecords] = useState<PlayRecord[]>([]);
+
+  return (
+    <div className="flex-column">
+      <h1>Sesión de hoy</h1>
+      <div className="flex-row rhythm-h-16">
+        <RecorderView onNewRecord={(r) => setRecords([...records, r])} />
+        <div className="flex-column flex-grow">
+          <h3>Interpretaciones</h3>
+          <RecordingList records={records} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+interface RecorderViewProps {
+  onNewRecord(record: PlayRecord): void;
+}
+
+function RecorderView({ onNewRecord }: RecorderViewProps) {
   const [title, setTitle] = useState("<untitled>");
   const [start, setStart] = useState<Date>(new Date());
   const [recording, setRecording] = useState(false);
-  const [records, setRecords] = useState<PlayRecord[]>([]);
   const [recorder, setRecorder] = useState<Recorder | undefined>(undefined);
 
   useEffect(function () {
@@ -21,53 +41,51 @@ export function SessionPage() {
   }, []);
 
   return (
-    <div className="flex-column">
-      <h1>Sesión de hoy</h1>
-      <div className="flex-row rhythm-h-16">
-        <div className="flex-column flex-grow">
-          <label>Título</label>
-          <input
-            onChange={(e) => setTitle(e.target.value)}
-            type="text"
-            value={title}
-          />
-          <div>
-            {recorder && (
-              <button
-                onClick={async () => {
-                  if (!recording) {
-                    setStart(new Date());
-                    recorder.start();
-                  } else {
-                    const audioBlob = await recorder.stop();
-                    setRecords([
-                      ...records,
-                      { start, end: new Date(), title, audioBlob },
-                    ]);
-                  }
+    <div className="flex-column flex-grow">
+      <label>Título</label>
+      <input
+        onChange={(e) => setTitle(e.target.value)}
+        type="text"
+        value={title}
+      />
+      <div>
+        {recorder && (
+          <button
+            onClick={async () => {
+              if (!recording) {
+                setStart(new Date());
+                recorder.start();
+              } else {
+                const audioBlob = await recorder.stop();
+                onNewRecord({ start, end: new Date(), title, audioBlob });
+              }
 
-                  setRecording(!recording);
-                }}
-              >
-                {recording ? "Terminar" : "Empezar"}
-              </button>
-            )}
-          </div>
-        </div>
-        <div className="flex-column flex-grow">
-          <h3>Interpretaciones</h3>
-          {records.map((record) => (
-            <div className="flex-row" key={record.start.toUTCString()}>
-              {record.title}&nbsp;hora&nbsp;{record.start.toLocaleTimeString()}
-              &nbsp;duración&nbsp;
-              {diffDateToDuration(record.start, record.end)}
-              <button onClick={() => playAudio(record.audioBlob)}>
-                Escuchar
-              </button>
-            </div>
-          ))}
-        </div>
+              setRecording(!recording);
+            }}
+          >
+            {recording ? "Terminar" : "Empezar"}
+          </button>
+        )}
       </div>
+    </div>
+  );
+}
+
+interface RecordingListProps {
+  records: PlayRecord[];
+}
+
+function RecordingList({ records }: RecordingListProps) {
+  return (
+    <div className="flex-column">
+      {records.map((record) => (
+        <div className="flex-row" key={record.start.toUTCString()}>
+          {record.title}&nbsp;hora&nbsp;{record.start.toLocaleTimeString()}
+          &nbsp;duración&nbsp;
+          {diffDateToDuration(record.start, record.end)}
+          <button onClick={() => playAudio(record.audioBlob)}>Escuchar</button>
+        </div>
+      ))}
     </div>
   );
 }
